72466cca9f276325ab09b4e89f03c5e9cdd6067c
perry <perry@channel.io>
2024-09-23 02:40:21 +0900

    feat: add exception filter

--- packages/api/src/main.ts
+++ packages/api/src/main.ts
@@ -1,9 +1,34 @@
 import { NestFactory } from "@nestjs/core";
 import { env } from "@kamf-safety/api/env";
+import { HttpException } from "@nestjs/common";
+import { ZodError } from "zod";
 import { AppModule } from "./app.module";
+import {
+  HttpExceptionFilter,
+  UnexpectedExceptionFilter,
+  ZodErrorFilter,
+} from "./common/util/exception.filter";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  await app.listen(env.SERVER_PORT);
+  // localhost에서의 cors 해결
+  if (process.env.NODE_ENV === "development") {
+    app.enableCors({
+      origin: `http://localhost:${process.env.CLIENT_PORT}`,
+      credentials: true,
+    });
+    app.useGlobalFilters(
+      new ZodErrorFilter<ZodError>(),
+      new HttpExceptionFilter<HttpException>(),
+    );
+  } else {
+    app.useGlobalFilters(
+      new UnexpectedExceptionFilter(),
+      new ZodErrorFilter<ZodError>(),
+      new HttpExceptionFilter<HttpException>(),
+    );
+  }
   await app.listen(env.SERVER_PORT);
 }
 bootstrap();
