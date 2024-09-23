b8e2a6744796792fc0363b4edeb1b7eceb58f501
perry <perry@channel.io>
2024-09-24 05:22:05 +0900

    feat: user module

--- packages/api/src/app.module.ts
+++ packages/api/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { DrizzleModule } from "./drizzle/drizzle.module";
+import { UserModule } from "./feature/user/user.module";
 
 @Module({
-  imports: [DrizzleModule],
+  imports: [DrizzleModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
