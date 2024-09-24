85a50b9757b6a09bbfa9233fbaf7533ed1b1c7a5
Woojeong Choi <gldeel@kaist.ac.kr>
2024-09-25 01:58:55 +0900

    feat: impl main page

--- packages/web/src/app/page.tsx
+++ packages/web/src/app/page.tsx
@@ -3,27 +3,34 @@
 import React from "react";
 import Image from "next/image";
 
-import map from "@kamf-safety/web/assets/map.png";
-import timetable from "@kamf-safety/web/assets/timetable.png";
+import map from "@kamf-safety/web/assets/map_kr.png";
+import timetable from "@kamf-safety/web/assets/timetable_kr.png";
 import styled from "styled-components";
+import Typography from "../common/components/Typography";
 
 const MapPageBackground = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  gap: 32px;
+  gap: 16px;
 `;
 
 const MapPage: React.FC = () => (
   <MapPageBackground>
-    <Image
-      src={map}
-      alt="KAMF map"
-      style={{ width: "100%", maxWidth: "600px", height: "auto" }}
-    />
+    <Typography fs={32} color="WHITE" fw="BOLD">
+      TIMETABLE
+    </Typography>
     <Image
       src={timetable}
       alt="KAMF timetable"
+      style={{ width: "100%", maxWidth: "600px", height: "auto" }}
+    />
+    <Typography fs={32} color="WHITE" fw="BOLD">
+      MAP
+    </Typography>
+    <Image
+      src={map}
+      alt="KAMF map"
       style={{ width: "100%", maxWidth: "600px", height: "auto" }}
     />
   </MapPageBackground>
