8f2789637c40ad860ffcb9c475921e6a69ccee68
perry <perry@channel.io>
2024-09-25 01:31:46 +0900

    feat: apiUser001

--- packages/api/src/feature/user/user.controller.ts
+++ packages/api/src/feature/user/user.controller.ts
@@ -1,7 +1,20 @@
-import { Controller } from "@nestjs/common";
+import { Body, Controller, Post, UsePipes } from "@nestjs/common";
+import { ZodPipe } from "@kamf-safety/api/common/pipe/zod.pipe";
+import apiUser001, {
+  ApiUser001RequestBody,
+  ApiUser001ResponseOK,
+} from "@kamf-safety/interface/api/user/apiUser001";
 import { UserService } from "./user.service";
 
 @Controller()
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @Post("/login")
+  @UsePipes(new ZodPipe(apiUser001))
+  async postLogin(
+    @Body() body: ApiUser001RequestBody,
+  ): Promise<ApiUser001ResponseOK> {
+    return this.userService.postLogin(body);
+  }
 }
--- packages/api/src/feature/user/user.service.ts
+++ packages/api/src/feature/user/user.service.ts
@@ -1,7 +1,26 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, NotFoundException } from "@nestjs/common";
+import {
+  ApiUser001RequestBody,
+  ApiUser001ResponseOK,
+} from "@kamf-safety/interface/api/user/apiUser001";
 import { UserRepository } from "./user.repository";
 
 @Injectable()
 export class UserService {
   constructor(private readonly userRepository: UserRepository) {}
+
+  async postLogin(body: ApiUser001RequestBody): Promise<ApiUser001ResponseOK> {
+    const user = await this.userRepository.getUserByIdAndPassword(
+      body.studentId,
+      body.password,
+    );
+
+    if (!user) {
+      throw new NotFoundException("User not found");
+    }
+
+    return {
+      userId: user.id,
+    };
+  }
 }
